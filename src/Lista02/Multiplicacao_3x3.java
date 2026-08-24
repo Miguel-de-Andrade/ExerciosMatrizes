52e7980aa649f33ff746152c40ebe743688fc9ef
MigzTe <migueldeandradeee@gmail.com>
2026-08-23 21:45:46 -0300

    Exercício adicionado

--- src/Lista02/Multiplicacao_3x3.java
+++ src/Lista02/Multiplicacao_3x3.java
@@ -2,6 +2,18 @@ package Lista02;
 
 public class Multiplicacao_3x3 {
     public static void main(String[] args) {
+        int[][] A = {
+                {3, 1, 6},
+                {3, 6, 7},
+                {4, 3, 1}
+        };
+
+        int[][] B = {
+                {1, 5, 4},
+                {1, 2, 6},
+                {9, 1, 3}
+        };
+
 
     }
 }
