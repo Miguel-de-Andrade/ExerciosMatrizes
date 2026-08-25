032557d03d3b5490bfbafe2bd0b961422d7a4e4f
MigzTe <migueldeandradeee@gmail.com>
2026-08-25 11:00:16 -0300

    Realizando exercício

--- src/Lista02/Multiplicacao_3x3.java
+++ src/Lista02/Multiplicacao_3x3.java
@@ -2,17 +2,38 @@ package Lista02;
 
 public class Multiplicacao_3x3 {
     public static void main(String[] args) {
-        int[][] A = {
-                {3, 1, 6},
-                {3, 6, 7},
-                {4, 3, 1}
+
+        //Declara a matriz A 2x4
+
+        int [] [] A = {
+                {2, 6, 1, 8},
+                {3, 5, 4, 1}
         };
 
-        int[][] B = {
-                {1, 5, 4},
-                {1, 2, 6},
-                {9, 1, 3}
+        //Declara a matriz B 4x2
+
+        int [][] B = {
+                {3, 6},
+                {1, 7},
+                {9, 8},
+                {1, 3}
         };
+
+        //Declara a matriz C que vai armazenar o resultado da multiplicação A x B
+
+        int [][] C = new int[2][2];
+
+        //Realizando a multiplicação entre as matrizes A e B
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                for (int k = 0; k < 4; k++) {
+
+                }
+
+            }
+
+        }
 
 
     }
